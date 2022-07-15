bfe86ebf909d7dae0b13eeb44528282db577052a
Gabriel <bellsena@outlook.com>
2022-07-14 21:06:51 -0300

    Desacoplando o Component State com headless em React

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,9 @@
 import { FilterButton } from './components/FilterButton'
-import { ISmartphone, SmartphoneItem } from './components/SmartphoneItem'
-import { useEffect, useState } from 'react'
-
-function paramsToString(storage?: string, manufacturer?: string) {
-  let params = {}
-
-  if (storage) {
-    params = { ...params, storage }
-  }
-
-  if (manufacturer) {
-    params = { ...params, manufacturer }
-  }
-
-  return params;
-}
+import { SmartphoneItem } from './components/SmartphoneItem'
+import { useSmartphones } from './hooks/useSmartphones'
 
 function App() {
-  const [smartphones, setSmartphone] = useState<ISmartphone[]>([])
-  const [storage, setStorage] = useState<string>()
-  const [manufacturer, setManufactur] = useState<string>()
-
-  useEffect(() => {
-    const paramsString = new URLSearchParams(paramsToString(storage, manufacturer))
-    console.log(paramsString.toString());
-    
-    fetch(`http://localhost:3333/smartphones?${paramsString}`).then(async (res) => {
-      const data = await res.json()
-
-      console.log(data)
-
-      setSmartphone(data)
-    })
-  }, [storage, manufacturer])
-
+  const { setManufactur, setStorage, smartphones } = useSmartphones()
   return (
     <div className="">
       <section className="max-w-5xl py-24 m-auto mt-12 shadow-lg">
@@ -41,7 +11,7 @@ function App() {
           Celulares a Preço de Fábrica
         </h1>
 
-        <pre>{JSON.stringify({ manufacturer, storage }, null, 2)}</pre>
+        {/* <pre>{JSON.stringify({ manufacturer, storage }, null, 2)}</pre> */}
 
         <div className="flex w-full mt-12 space-x-6">
           <div className="flex flex-col items-center w-full p-6 shadow">
@@ -77,7 +47,7 @@ function App() {
         </div>
 
         <div className="flex flex-wrap justify-around mt-12 space-y-12">
-          {smartphones.map((smartphone) => (
+          {smartphones?.map((smartphone) => (
             <SmartphoneItem key={smartphone.id} data={smartphone} />
           ))}
         </div>
---
+++ src/hooks/useSmartphones.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+import { ISmartphone } from '../components/SmartphoneItem'
+import { useFetch } from './useFetch'
+
+function paramsToString(storage?: string, manufacturer?: string) {
+  let params = {}
+
+  if (storage) {
+    params = { ...params, storage }
+  }
+
+  if (manufacturer) {
+    params = { ...params, manufacturer }
+  }
+
+  return params
+}
+
+export const useSmartphones = () => {
+  const [storage, setStorage] = useState<string>()
+  const [manufacturer, setManufactur] = useState<string>()
+
+  const paramsString = new URLSearchParams(
+    paramsToString(storage, manufacturer)
+  )
+
+  const { response } = useFetch<ISmartphone[]>(
+    `http://localhost:3333/smartphones?${paramsString}`
+  )
+
+  return {
+    smartphones: response,
+    setManufactur,
+    setStorage,
+  }
+}
